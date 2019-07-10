423c6726fd0d21332f751de025192e56d58863ef
cuonglv <levietcuong0297@gmail.com>
2019-07-10 23:04:15 +0700

    init project

---
+++ data/products.js
@@ -0,0 +1,433 @@
+export const products = [{
+    id: 'product_form_27700057307',
+    img: 'https://www.vinabook.com/images/thumbnails/product/115x/138440_tinh-khong-nhu-la-mo.jpg',
+    title: 'Tình Không Như Là Mơ',
+    author: 'Phan An',
+    oldPrice: '64.000',
+    finalPrice: '39.000',
+    sales: '-39%'
+  },
+  {
+    id: 'product_form_27700050948',
+    img: 'https://www.vinabook.com/images/thumbnails/product/115x/44405_so-tay-tieng-anh-danh-cho-tai-xe-english-for-drivers-kem-1-cd.jpg',
+    title: 'Sổ Tay Tiếng Anh Dành Cho Tài Xế - English For Drivers (Kèm 1 CD)',
+    author: 'Trường Hải - Hoàng Yến',
+    oldPrice: '32.000',
+    finalPrice: '16.000',
+    sales: '-50%'
+  },
+  {
+    id: 'product_form_27700033341',
+    img: 'https://www.vinabook.com/images/thumbnails/product/115x/26871_doi-thoai-voi-tran-khai-ca.jpg',
+    title: 'Đối Thoại Với Trần Khải Ca',
+    author: 'Lý Nhĩ Uy',
+    oldPrice: '40.000',
+    finalPrice: '12.000',
+    sales: '-70%'
+  },
+  {
+    id: 'product_form_27700030319',
+    img: 'https://www.vinabook.com/images/thumbnails/product/115x/23862_huong-dan-hoc-windows-xp-can-ban.jpg',
+    title: 'Hướng Dẫn Học Windows XP Căn Bản',
+    author: 'Hoàng Phương',
+    oldPrice: '36.000',
+    finalPrice: '11.000',
+    sales: '-69%'
+  },
+  {
+    id: 'product_form_27700029573',
+    img: 'https://www.vinabook.com/images/thumbnails/product/115x/23125_cung-coi-nguon-tap-2.jpg',
+    title: 'Cùng Cội Nguồn - Tập 2',
+    author: 'Nguyễn Văn Lân',
+    oldPrice: '44.000',
+    finalPrice: '14.000',
+    sales: '-68%'
+  },
+  {
+    id: 'product_form_27700029508',
+    img: 'https://www.vinabook.com/images/thumbnails/product/115x/23064_dong-vat-trong-thanh-pho-tai-ban-08082008.jpg',
+    title: 'Động Vật Trong Thành Phố - Tái bản 08/08/2008',
+    author: 'Nguyễn Vĩnh Nguyên',
+    oldPrice: '29.000',
+    finalPrice: '9.000',
+    sales: '-69%'
+  },
+  {
+    id: 'product_form_27500087624',
+    img: 'https://www.vinabook.com/images/thumbnails/product/115x/338583_bo-sach-giao-khoa-lop-12-2019-bai-hoc-bo-14-cuon.jpg',
+    title: 'Bộ Sách Giáo Khoa Lớp 12 (2019) - Bài Học (Bộ 14 Cuốn)',
+    author: 'Nhiều tác giả',
+    oldPrice: '180.000',
+    finalPrice: '171.000',
+    sales: '-5%'
+  },
+  {
+    id: 'product_form_27500087623',
+    img: 'https://www.vinabook.com/images/thumbnails/product/115x/338560_bo-sach-giao-khoa-lop-11-2019-bai-hoc-bo-14-cuon.jpg',
+    title: 'Bộ Sách Giáo Khoa Lớp 11 (2019) - Bài Học (Bộ 14 Cuốn)',
+    author: 'Nhiều tác giả',
+    oldPrice: '169.000',
+    finalPrice: '161.000',
+    sales: '-5%'
+  },
+  {
+    id: 'product_form_27500087622',
+    img: 'https://www.vinabook.com/images/thumbnails/product/115x/338526_bo-sach-giao-khoa-lop-10-2019-bai-hoc-bo-14-cuon.jpg',
+    title: 'Bộ Sách Giáo Khoa Lớp 10 (2019) - Bài Học (Bộ 14 Cuốn)',
+    author: 'Nhiều tác giả',
+    oldPrice: '164.000',
+    finalPrice: '156.000',
+    sales: '-5%'
+  },
+  {
+    id: 'product_form_27500087621',
+    img: 'https://www.vinabook.com/images/thumbnails/product/115x/338466_bo-sach-giao-khoa-lop-9-2019-bai-hoc-bo-12-cuon.jpg',
+    title: 'Bộ Sách Giáo Khoa Lớp 9 (2019) - Bài Học (Bộ 12 Cuốn)',
+    author: 'Nhiều tác giả',
+    oldPrice: '136.000',
+    finalPrice: '129.000',
+    sales: '-5%'
+  },
+  {
+    id: 'product_form_27500087620',
+    img: 'https://www.vinabook.com/images/thumbnails/product/115x/338494_bo-sach-giao-khoa-lop-8-2019-bai-hoc-bo-13-cuon.jpg',
+    title: 'Bộ Sách Giáo Khoa Lớp 8 (2019) - Bài Học (Bộ 13 Cuốn)',
+    author: 'Nhiều Tác Giả',
+    oldPrice: '149.000',
+    finalPrice: '142.000',
+    sales: '-5%'
+  },
+  {
+    id: 'product_form_27500087619',
+    img: 'https://www.vinabook.com/images/thumbnails/product/115x/338481_bo-sach-giao-khoa-lop-7-2019-bai-hoc-bo-12-cuon.jpg',
+    title: 'Bộ Sách Giáo Khoa Lớp 7 (2019) - Bài Học (Bộ 12 Cuốn)',
+    author: 'Nhiều tác giả',
+    oldPrice: '134.000',
+    finalPrice: '127.000',
+    sales: '-5%'
+  },
+  {
+    id: 'product_form_25700086978',
+    img: 'https://www.vinabook.com/images/thumbnails/product/115x/333375_con-cao-hung-dang-gia-ngan-vang.jpg',
+    title: 'Cơn Cao Hứng Đáng Giá Ngàn Vàng',
+    author: 'Lê Thiếu Nhơn',
+    oldPrice: '70.000',
+    finalPrice: '56.000',
+    sales: '-20%'
+  },
+  {
+    id: 'product_form_25700086913',
+    img: 'https://www.vinabook.com/images/thumbnails/product/115x/332763_chung-ta-deu-so-ton-thuong.jpg',
+    title: 'Chúng Ta Đều Sợ Tổn Thương',
+    author: 'Đới Quân',
+    oldPrice: '86.000',
+    finalPrice: '69.000',
+    sales: '-20%'
+  },
+  {
+    id: 'product_form_25700086810',
+    img: 'https://www.vinabook.com/images/thumbnails/product/115x/331946_khon-ngoan-vua-du-lam-chu-cuoc-doi.jpg',
+    title: 'Khôn Ngoan Vừa Đủ, Làm Chủ Cuộc Đời',
+    author: 'Vũ Mãn Lan Giang',
+    oldPrice: '99.000',
+    finalPrice: '79.000',
+    sales: '-20%'
+  },
+  {
+    id: 'product_form_25700086751',
+    img: 'https://www.vinabook.com/images/thumbnails/product/115x/331396_hoi-ky-tien-si-le-tham-duong.jpg',
+    title: 'Hồi Ký Tiến Sĩ Lê Thẩm Dương',
+    author: 'Nguyễn Tuấn Anh',
+    oldPrice: '280.000',
+    finalPrice: '238.000',
+    sales: '-15%'
+  },
+  {
+    id: 'product_form_25700086701',
+    img: 'https://www.vinabook.com/images/thumbnails/product/115x/331060_em-luon-dung-em-yeu.jpg',
+    title: 'Em Luôn Đúng, Em Yêu!',
+    author: 'Dung Quang',
+    oldPrice: '145.000',
+    finalPrice: '116.000',
+    sales: '-20%'
+  },
+  {
+    id: 'product_form_25700086393',
+    img: 'https://www.vinabook.com/images/thumbnails/product/115x/329199_chua-du-co-don-de-yeu-chang.jpg',
+    title: 'Chưa Đủ Cô Đơn Để Yêu Chăng?',
+    author: 'G  ',
+    oldPrice: '87.000',
+    finalPrice: '70.000',
+    sales: '-20%'
+  },
+  {
+    id: 'product_form_25800088017',
+    img: 'https://www.vinabook.com/images/thumbnails/product/115x/341971_nu-cuoi-cua-shoko.jpg',
+    title: 'Nụ Cười Của Shoko',
+    author: 'Choi Eun-Young',
+    oldPrice: '90.000',
+    finalPrice: '72.000',
+    sales: '-20%'
+  },
+  {
+    id: 'product_form_25800088016',
+    img: 'https://www.vinabook.com/images/thumbnails/product/115x/341975_yeu-em-nhu-yeu-sinh-menh.jpg',
+    title: 'Yêu Em Như Yêu Sinh Mệnh',
+    author: 'Vương Tiểu Ba',
+    oldPrice: '81.000',
+    finalPrice: '65.000',
+    sales: '-20%'
+  },
+  {
+    id: 'product_form_25800088012',
+    img: 'https://www.vinabook.com/images/thumbnails/product/115x/341909_co-gai-trong-suong-mu.jpg',
+    title: 'Cô Gái Trong Sương Mù',
+    author: 'Donato Carrisi',
+    oldPrice: '135.000',
+    finalPrice: '108.000',
+    sales: '-20%'
+  },
+  {
+    id: 'product_form_25800088010',
+    img: 'https://www.vinabook.com/images/thumbnails/product/115x/341857_gone-girl-co-gai-mat-tich-tai-ban-2019.jpg',
+    title: 'Gone Girl - Cô Gái Mất Tích (Tái Bản 2019)',
+    author: 'Gillian Flynn',
+    oldPrice: '199.000',
+    finalPrice: '159.000',
+    sales: '-20%'
+  },
+  {
+    id: 'product_form_25800088008',
+    img: 'https://www.vinabook.com/images/thumbnails/product/115x/341841_nam-thang-nhoc-nhan-nam-thang-nho-thuong.jpg',
+    title: 'Năm Tháng Nhọc Nhằn - Năm Tháng Nhớ Thương',
+    author: 'Ma Văn Kháng',
+    oldPrice: '169.000',
+    finalPrice: '135.000',
+    sales: '-20%'
+  },
+  {
+    id: 'product_form_25800087997',
+    img: 'https://www.vinabook.com/images/thumbnails/product/115x/341662_riku-va-chise-chang-trai-phan-chia-the-gioi-va-co-gai-trong-biet-thu.jpg',
+    title: 'Riku Và Chise - Chàng Trai Phân Chia Thế Giới Và Cô Gái Trong Biệt Thự',
+    author: 'Nomura Mizuki',
+    oldPrice: '85.000',
+    finalPrice: '68.000',
+    sales: '-20%'
+  },
+  {
+    id: 'product_form_22300087935',
+    img: 'https://www.vinabook.com/images/thumbnails/product/115x/340915_instastyle-xay-dung-thuong-hieu-ca-nhan-tren-instagram.jpg',
+    title: '#InstaStyle - Xây Dựng Thương Hiệu Cá Nhân Trên Instagram',
+    author: 'Aimee Song',
+    oldPrice: '139.000',
+    finalPrice: '111.000',
+    sales: '-20%'
+  },
+  {
+    id: 'product_form_22300087971',
+    img: 'https://www.vinabook.com/images/thumbnails/product/115x/341493_nguoi-ban-hang-vi-dai-nhat-the-gioi-tap-1-tai-ban-2019.jpg',
+    title: 'Người Bán Hàng Vĩ Đại Nhất Thế Giới - Tập 1 (Tái Bản 2019)',
+    author: 'Og Mandino',
+    oldPrice: '58.000',
+    finalPrice: '46.000',
+    sales: '-21%'
+  },
+  {
+    id: 'product_form_22300087972',
+    img: 'https://www.vinabook.com/images/thumbnails/product/115x/341500_khong-bao-gio-la-that-bai-tat-ca-la-thu-thach-tai-ban-2019.jpg',
+    title: 'Không Bao Giờ Là Thất Bại! Tất Cả Là Thử Thách (Tái Bản 2019)',
+    author: 'Chung Ju Yung',
+    oldPrice: '98.000',
+    finalPrice: '78.000',
+    sales: '-20%'
+  },
+  {
+    id: 'product_form_22300087996',
+    img: 'https://www.vinabook.com/images/thumbnails/product/115x/341642_the-he-y-len-ngoi.jpg',
+    title: 'Thế Hệ Y Lên Ngôi',
+    author: 'Maddie Grant',
+    oldPrice: '79.000',
+    finalPrice: '63.000',
+    sales: '-20%'
+  },
+  {
+    id: 'product_form_22300088006',
+    img: 'https://www.vinabook.com/images/thumbnails/product/115x/341788_de-xay-dung-doanh-nghiep-hieu-qua-tai-ban-2019.jpg',
+    title: 'Để Xây Dựng Doanh Nghiệp Hiệu Quả (Tái Bản 2019)',
+    author: 'Michael E. Gerber',
+    oldPrice: '119.000',
+    finalPrice: '95.000',
+    sales: '-20%'
+  },
+  {
+    id: 'product_form_22300088009',
+    img: 'https://www.vinabook.com/images/thumbnails/product/115x/341849_thiet-ke-lay-nguoi-dung-lam-trung-tam.jpg',
+    title: 'Thiết Kế Lấy Người Dùng Làm Trung Tâm',
+    author: 'Don Norman',
+    oldPrice: '199.000',
+    finalPrice: '159.000',
+    sales: '-20%'
+  },
+  {
+    id: 'product_form_26200088018',
+    img: 'https://www.vinabook.com/images/thumbnails/product/115x/342012_tuoi-tho-tim-thay.jpg',
+    title: 'Tuổi Thơ Tìm Thấy',
+    author: 'Carl Honoré',
+    oldPrice: '195.000',
+    finalPrice: '156.000',
+    sales: '-20%'
+  },
+  {
+    id: 'product_form_26200087993',
+    img: 'https://www.vinabook.com/images/thumbnails/product/115x/341650_thien-tap-cho-con.jpg',
+    title: 'Thiền Tập Cho Con',
+    author: 'David Fontana',
+    oldPrice: '79.000',
+    finalPrice: '63.000',
+    sales: '-20%'
+  },
+  {
+    id: 'product_form_26200087980',
+    img: 'https://www.vinabook.com/images/thumbnails/product/115x/341531_gioi-han-cho-con-ban.jpg',
+    title: 'Giới Hạn Cho Con Bạn',
+    author: 'John Townsend',
+    oldPrice: '119.000',
+    finalPrice: '95.000',
+    sales: '-20%'
+  },
+  {
+    id: 'product_form_26200087948',
+    img: 'https://www.vinabook.com/images/thumbnails/product/115x/341483_ky-luat-khong-nuoc-mat.jpg',
+    title: 'Kỷ Luật Không Nước Mắt',
+    author: 'Peter L. Stavinoha....',
+    oldPrice: '128.000',
+    finalPrice: '102.000',
+    sales: '-20%'
+  },
+  {
+    id: 'product_form_26200087853',
+    img: 'https://www.vinabook.com/images/thumbnails/product/115x/341114_de-con-duoc-chich.jpg',
+    title: 'Để Con Được Chích',
+    author: 'Nhiều Tác Giả',
+    oldPrice: '108.000',
+    finalPrice: '86.000',
+    sales: '-20%'
+  },
+  {
+    id: 'product_form_26200087839',
+    img: 'https://www.vinabook.com/images/thumbnails/product/115x/340203_277-loi-khuyen-day-con-cua-giao-su-shichida.jpg',
+    title: '277 Lời Khuyên Dạy Con Của Giáo Sư Shichida',
+    author: 'Makoto Shichida',
+    oldPrice: '108.000',
+    finalPrice: '86.000',
+    sales: '-20%'
+  },
+  {
+    id: 'product_form_26300087968',
+    img: 'https://www.vinabook.com/images/thumbnails/product/115x/341434_biet-doi-sieu-quay-tap-1-bi-mat-ve-de-che-khung-long.jpg',
+    title: 'Biệt Đội Siêu Quậy - Tập 1: Bí Mật Về "Đế Chế Khủng Long"',
+    author: 'Nhiều tác giả',
+    oldPrice: '38.000',
+    finalPrice: '32.000',
+    sales: '-16%'
+  },
+  {
+    id: 'product_form_26300087818',
+    img: 'https://www.vinabook.com/images/thumbnails/product/115x/340043_lop-hoc-mat-ngu-tuyen-tap-dac-biet-vol3-tang-kem-moc-khoa.jpg',
+    title: 'Lớp Học Mật Ngữ - Tuyển Tập Đặc Biệt Vol.3 (Tặng Kèm Móc Khóa)',
+    author: 'B.R.O',
+    oldPrice: '90.000',
+    finalPrice: '77.000',
+    sales: '-14%'
+  },
+  {
+    id: 'product_form_26300087966',
+    img: 'https://www.vinabook.com/images/thumbnails/product/115x/341264_cuon-sach-lon-ruc-ro-ve-bac-si.jpg',
+    title: 'Cuốn Sách Lớn Rực Rỡ Về Bác Sĩ',
+    author: 'Liesbet Slegers',
+    oldPrice: '48.000',
+    finalPrice: '38.000',
+    sales: '-21%'
+  },
+  {
+    id: 'product_form_26300087976',
+    img: 'https://www.vinabook.com/images/thumbnails/product/115x/341515_kien-tri-khong-bo-cuoc-song-ngu-tai-ban-2019.jpg',
+    title: 'Kiên Trì Không Bỏ Cuộc (Song Ngữ) - Tái Bản 2019',
+    author: 'Jennifer Moore-Mallinos',
+    oldPrice: '50.000',
+    finalPrice: '40.000',
+    sales: '-20%'
+  },
+  {
+    id: 'product_form_26300087973',
+    img: 'https://www.vinabook.com/images/thumbnails/product/115x/341510_bach-khoa-thu-larousse-nhung-phat-minh-lam-thay-doi-cuoc-doi-ban-tai-ban-2019.jpg',
+    title: 'Bách Khoa Thư Larousse - Những Phát Minh Làm Thay Đổi Cuộc Đời Bạn (Tái Bản 2019)',
+    author: 'Nhiều Tác Giả',
+    oldPrice: '130.000',
+    finalPrice: '104.000',
+    sales: '-20%'
+  },
+  {
+    id: 'product_form_26300088013',
+    img: 'https://www.vinabook.com/images/thumbnails/product/115x/341911_dem-sao.jpg',
+    title: 'Đếm Sao',
+    author: 'Lois Lowry',
+    oldPrice: '72.000',
+    finalPrice: '58.000',
+    sales: '-19%'
+  },
+  {
+    id: 'product_form_27000088003',
+    img: 'https://www.vinabook.com/images/thumbnails/product/115x/341794_mat-ma-tai-nang-tai-ban-2019.jpg',
+    title: 'Mật Mã Tài Năng (Tái Bản 2019)',
+    author: 'Daniel Coyle',
+    oldPrice: '129.000',
+    finalPrice: '103.000',
+    sales: '-20%'
+  },
+  {
+    id: 'product_form_27000088004',
+    img: 'https://www.vinabook.com/images/thumbnails/product/115x/341792_song-cham-lai-roi-moi-chuyen-se-on-thoi-tai-ban-2019.jpg',
+    title: 'Sống Chậm Lại Rồi Mọi Chuyện Sẽ Ổn Thôi (Tái Bản 2019)',
+    author: 'Alpha Books biên soạn',
+    oldPrice: '89.000',
+    finalPrice: '71.000',
+    sales: '-20%'
+  },
+  {
+    id: 'product_form_27000088005',
+    img: 'https://www.vinabook.com/images/thumbnails/product/115x/341790_gai-khon-khong-bao-gio-so-e-tai-ban-2019.jpg',
+    title: 'Gái Khôn Không Bao Giờ Sợ Ế (Tái Bản 2019)',
+    author: 'Marie Forleo',
+    oldPrice: '79.000',
+    finalPrice: '63.000',
+    sales: '-20%'
+  },
+  {
+    id: 'product_form_27000088007',
+    img: 'https://www.vinabook.com/images/thumbnails/product/115x/341786_nhung-quy-tac-trong-tinh-yeu-tai-ban-2019.jpg',
+    title: 'Những Quy Tắc Trong Tình Yêu (Tái Bản 2019)',
+    author: 'Richard Templar',
+    oldPrice: '139.000',
+    finalPrice: '111.000',
+    sales: '-20%'
+  },
+  {
+    id: 'product_form_27000088015',
+    img: 'https://www.vinabook.com/images/thumbnails/product/115x/341977_dam-thay-doi.jpg',
+    title: 'Dám Thay Đổi',
+    author: 'Susyn Reeve',
+    oldPrice: '119.000',
+    finalPrice: '95.000',
+    sales: '-20%'
+  },
+  {
+    id: 'product_form_27000088019',
+    img: 'https://www.vinabook.com/images/thumbnails/product/115x/342010_khong-viec-gi-phai-voi.jpg',
+    title: 'Không Việc Gì Phải Vội!',
+    author: 'Carl Honoré',
+    oldPrice: '209.000',
+    finalPrice: '167.000',
+    sales: '-20%'
+  }
+]
